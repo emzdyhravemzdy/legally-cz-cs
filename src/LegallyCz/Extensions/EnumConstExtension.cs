77fe7c2b14b82d624003ee3f7cc9b6794f16200e
mzdyhrave <info@hravemzdy.org>
2023-12-01 19:15:44 +0100

    Health

---
+++ src/LegallyCz/Extensions/EnumConstExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace HraveMzdy.LegallyCz.Extensions;
+
+internal static class EnumConstExtension
+{
+    public static string GetSymbol<T>(Int32 symbol) where T : struct, Enum
+    {
+        var enumValue = (T)Enum.ToObject(typeof(T), symbol);
+        return Enum.GetName<T>(enumValue) ?? "";
+    }
+    public static T GetEnum<T>(Int32 symbol, T empty) where T : struct, Enum
+    {
+        T? enumValue = (T)Enum.ToObject(typeof(T), symbol);
+        return (enumValue) ?? empty;
+    }
+    public static T GetEnum<T>(Int16 symbol, T empty) where T : struct, Enum
+    {
+        T? enumValue = (T)Enum.ToObject(typeof(T), symbol);
+        return (enumValue) ?? empty;
+    }
+}
--- src/LegallyCz/Types/ContractTerms.cs
+++ src/LegallyCz/Types/ContractTerms.cs
@@ -1,4 +1,6 @@
-﻿namespace HraveMzdy.LegallyCz.Types;
+﻿using HraveMzdy.LegallyCz.Extensions;
+
+namespace HraveMzdy.LegallyCz.Types;
 
 public enum WorkContractTerms : UInt16
 {
@@ -7,6 +9,15 @@ public enum WorkContractTerms : UInt16
     WORKTERM_CONTRACTER_T = 2,
     WORKTERM_PARTNER_STAT = 3,
 };
+
+public static class WorkContractTermsEnum
+{
+    public static WorkContractTerms Get(this Int32 symbol)
+    {
+        return EnumConstExtension.GetEnum<WorkContractTerms>((UInt16)symbol, WorkContractTerms.WORKTERM_EMPLOYMENT_1);
+    }
+}
+
 public enum WorkTaxingTerms : UInt16
 {
     TAXING_TERM_BY_CONTRACT = 0,
@@ -14,6 +25,14 @@ public enum WorkTaxingTerms : UInt16
     TAXING_TERM_AGREEM_TASK = 2,
     TAXING_TERM_STATUT_PART = 3,
 };
+public static class WorkTaxingTermsEnum
+{
+    public static WorkTaxingTerms Get(this Int32 symbol)
+    {
+        return EnumConstExtension.GetEnum<WorkTaxingTerms>((UInt16)symbol, WorkTaxingTerms.TAXING_TERM_BY_CONTRACT);
+    }
+}
+
 public enum WorkHealthTerms : UInt16
 {
     HEALTH_TERM_BY_CONTRACT = 0,
@@ -21,6 +40,14 @@ public enum WorkHealthTerms : UInt16
     HEALTH_TERM_AGREEM_WORK = 2,
     HEALTH_TERM_AGREEM_TASK = 3,
 };
+public static class WorkHealthTermsEnum
+{
+    public static WorkHealthTerms Get(this Int32 symbol)
+    {
+        return EnumConstExtension.GetEnum<WorkHealthTerms>((UInt16)symbol, WorkHealthTerms.HEALTH_TERM_BY_CONTRACT);
+    }
+}
+
 public enum WorkSocialTerms : UInt16
 {
     SOCIAL_TERM_BY_CONTRACT = 0,
@@ -30,3 +57,11 @@ public enum WorkSocialTerms : UInt16
     SOCIAL_TERM_SHORTS_DENY = 4,
     SOCIAL_TERM_AGREEM_TASK = 5,
 };
+public static class WorkSocialTermsEnum
+{
+    public static WorkSocialTerms Get(this Int32 symbol)
+    {
+        return EnumConstExtension.GetEnum<WorkSocialTerms>((UInt16)symbol, WorkSocialTerms.SOCIAL_TERM_BY_CONTRACT);
+    }
+}
+
